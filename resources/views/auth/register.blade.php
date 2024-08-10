3d959ade6c2176dfafe0e02bf575d0c394b3daa4
Albina <albinarybanchuk@gmail.com>
2024-08-11 01:58:59 +0300

    validations

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -6,22 +6,36 @@
             @csrf
             <div class="mb-4">
                 <label for="username">Username</label>
-                <input type="text" name="username" class="input">
+                <input type="text" name="username" value="{{ old('username')}}" class="input
+                @error('username') ring-red-500 @enderror">
+                @error('username')
+                    <p class="error">{{ $message}}</p>
+                @enderror
             </div>
 
             <div class="mb-4">
                 <label for="email">Email</label>
-                <input type="text" name="email" class="input">
+                <input type="text" name="email" value="{{ old('email')}}" class="input
+                @error('email') ring-red-500 @enderror">
+                @error('username')
+                    <p class="error">{{ $message}}</p>
+                @enderror
             </div>
 
             <div class="mb-4">
                 <label for="password">Password</label>
                 <input type="password" name="password" class="input">
+                @error('password')
+                    <p class="error">{{ $message}}</p>
+                @enderror
             </div>
 
             <div class="mb-4">
                 <label for="password_confirmation">Confirm Password</label>
                 <input type="password" name="password_confirmation" class="input">
+                @error('password')
+                    <p class="error">{{ $message}}</p>
+                @enderror
             </div>
 
             <button class="btn">Register</button>
